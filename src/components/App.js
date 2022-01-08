867748137294404d8899ee621a0989d582567a4b
JazzPants <leighton.l14@gmail.com>
2022-01-09 02:11:04 +1100

    Done!

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,57 @@ class App extends React.Component {
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: 'all',
+        // description: 'random',
+        // hello: 'world'
       }
     }
+  }
+
+  onChangeType = ({target: {value}}) => { //event.target.value
+    //change state on filters type
+    // ...this.state.filters, spread operator for more than one nested property/state
+    //or object in object like below because there is 
+    //only one property in filters otherwise it will replace all properties in filters with only one, called "type"
+    this.setState(
+      {filters: {
+        // ...this.state.filters, copy all properties then override type with new value
+        type: value
+      }
+    }
+    )
+  }
+
+  //get pets.js objects
+  fetchPets = () => {
+    //fetch all pets
+    let endpoint = '/api/pets'
+    if(this.state.filters.type !== "all") { //double check URLs for failed get/post requests!
+      endpoint = endpoint + "?type=" + this.state.filters.type
+    }
+
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(pets => this.setState({
+      pets: pets
+    }))
+  }
+
+  //check if pet chosen has already been adopted
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet
+    })
+    /* Other way of writing this if statement
+    if (pet.id === petId) {
+      return {...pet, isAdopted: true}
+    } else {
+      return pet
+    }
+    */
+    this.setState({//first level of the object, react will copy over, but you can still use spread operator ...
+      pets: pets
+    })
   }
 
   render() {
@@ -24,10 +72,13 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType ={this.onChangeType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              {/* 
+              onAdoptPet function changes isAdopted to true
+              Pass the pets array we fetched to Petbrowser using a pets prop */}
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,13 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+      //loop through pet array data and for each pet, render a pet component
+    const petComponent = this.props.pets.map((pet, i) => ( //use array index for identification "i"
+    <Pet key={i} pet={pet} onAdoptPet={this.props.onAdoptPet}></Pet>)
+    )
+    return <div className="ui cards">{petComponent}</div>
   }
 }
 
